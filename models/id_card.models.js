24bb7b796f8e8e1d7890a065e87e3781278cf568
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-05-25 00:31:50 +0500

    id card Model

---
+++ models/id_card.models.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const idCardSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    digitalIdNumber: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    issueDate: {
+        type: Date,
+        default: Date.now
+    },
+    qrCodeUrl: {
+        type: String,
+        required: true
+    }, // URL to QR code image or data
+    idCardUrl: {
+        type: String,
+        required: true
+    }, // URL or path to generated PDF/Image of ID card
+});
+
+module.exports = mongoose.model('IdCard', idCardSchema);
