67b503cc86d95b461c18ad403424387e833781cb
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-05-25 00:20:26 +0500

    Chat Model

---
+++ models/chat.models.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const chatSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    platform: {
+        type: String,
+        enum: ['WhatsApp', 'Facebook', 'Instagram'],
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    response: {
+        type: String,
+        required: true
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    },
+    intent: {
+        type: String,
+        default: null
+    }, // Dialogflow intent name if matched
+    generatedBy: {
+        type: String,
+        enum: ['Dialogflow', 'Gemini'],
+        required: true
+    },
+});
+
+module.exports = mongoose.model('Chat', chatSchema);
