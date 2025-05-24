16814625ab5cee07797f293100702a9c69ef5cf5
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-05-24 23:47:32 +0500

    Server

---
+++ db/connection.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const instance = await mongoose.connect(process.env.MONGO_URI, {
+            dbName: "SMIT-AI-HACKATHON"
+        });
+        console.log(`✅ MongoDB Connected: ${instance.connection.host}`);
+    } catch (error) {
+        console.error(`❌ MongoDB Connection Error: ${error.message}`);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
